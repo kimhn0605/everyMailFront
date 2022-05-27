9326b953cb1bf32414e0a6c5197773b9cc04feac
Crmal <pjm2207@naver.com>
2022-05-27 13:07:12 +0900

    feat: login jwt기능 추가

--- static/js/cookie.js
+++ static/js/cookie.js
@@ -1,35 +1,50 @@
-const { cookie } = require("express/lib/response");
-
-// 쿠키에 값 설정하기
-Cookies.set('X-Acess Token', 'response.data.jwt');
-
-// 유효기간과 함께 쿠키에 값 설정하기
-Cookies.set('name', 'value', { expires: 7 });
-
-// 쿠키 값 가져오기
-Cookies.get('name'); // 'value'
-Cookies.get('nothing'); // undefined
-
-// 모든 쿠키 값 가져오기
-Cookies.get(); // { name: 'value' }
-
-Cookies.remove('name');
-
 // 로그인
-fetch("http://ec2-54-167-22-41.compute-1.amazonaws.com/api/auth/sign-in", {
-  method: "POST",
-  body: JSON.stringify({
-    "nickName": user_id,
-	"password": user_pw,
-  }),
-}).then((response) => Cookies.set('X-Acess Token', 'response.data.jwt'));
+async function login() {
+  var user = document.getElementById("user_id");
+  var userpw = document.getElementById("user_pw");
+  try {
+    await fetch("http://54.167.22.41/api/auth/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        nickName: user.value,
+        password: userpw.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => Cookies.set("X-Access-Token", result.data.jwt))
+      .catch((error) => console.log("error: ", error));
+  } catch (error) {
+    console.log(error);
+  }
+}
 
+async function test() {
+  await fetch("https://api.github.com/users/xiaotian/repos")
+    .then(
+      (resp) => resp.json() // this returns a promise
+    )
+    .then((repos) => {
+      for (const repo of repos) {
+        console.log(repo.name);
+      }
+    })
+    .catch((ex) => {
+      console.error(ex);
+    });
+}
 
-// 시간표 전체 불러오기
-fetch("http://ec2-54-167-22-41.compute-1.amazonaws.com/api/timeTable/entire", {
-  method: "GET",
-  body: JSON.stringify({
-    "nickName": user_id,
-	"password": user_pw,
-  }),
-}).then((response) => Cookies.set('X-Acess Token', 'response.data.jwt'));
+function createGist(opts) {
+  fetch("https://api.github.com/gists", {
+    method: "post",
+    body: JSON.stringify(opts),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      ChromeSamples.log("Created Gist:", data.html_url);
+    });
+}
