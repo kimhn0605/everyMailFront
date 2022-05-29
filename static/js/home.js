3fc867e3b51716532a255939608a9ebae5c7db00
Crmal <pjm2207@naver.com>
2022-05-29 12:19:16 +0900

    fix: 127.0.0.1 -> localhost변경

--- static/js/home.js
+++ static/js/home.js
@@ -33,5 +33,5 @@ function showSlides(n) {
 }
 
 function popUp() {
-  window.location.href = "http://127.0.0.1:5500/templates/login.html";
+  window.location.href = "http://localhost:5500/index.html";
 }
