ed8ce7d99600281f31077abff55b88351bd81718
Crmal <pjm2207@naver.com>
2022-05-29 10:54:34 +0900

    merge

--- static/js/mail.js
+++ static/js/mail.js
@@ -11,16 +11,6 @@ fetch("http://ec2-54-167-22-41.compute-1.amazonaws.com/api/user/mail", {
     console.log(data);
     var mailbox = document.getElementById("mailbox_list_contents");
     data.data.mailData.forEach((element) => {
-      var name = ["김성철", "배종우", "신서용", "강환일", "정재희"];
-      var subject = [
-        "파이썬",
-        "파이썬",
-        "회로이론",
-        "회로이론",
-        "인터넷 프로그래밍",
-      ];
-      var sPick = Math.floor(Math.random() * name.length);
-      console.log(element);
       mailbox.innerHTML += `<tr align="center">
                     <td>${element.createdAt}</td> 
                     <td>${element.receive}</td>
@@ -41,4 +31,36 @@ function buttonClick(ID) {
     "width=550px, height=550px, scrollbars=yes, menubar=no, location=yes"
   );
   Cookies.set("Number", ID);
+}
+
+function sendMail() {
+  fetch("http://ec2-54-167-22-41.compute-1.amazonaws.com/api/sendMail", {
+    method: "POST",
+    headers: {
+      "X-Access-Token": Cookies.get("X-Access-Token"),
+    },
+    body: JSON.stringify({
+      professor_email: document.getElementById("sendEmail").value,
+      subject: document.getElementById("sendEmail").value,
+      text: document.getElementById("sendEmail").value,
+      subject_name: "인터넷 프로그래밍",
+      receive: "정재희",
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      var mailbox = document.getElementById("mailbox_list_contents");
+      data.data.mailData.forEach((element) => {
+        mailbox.innerHTML += `<tr align="center">
+                      <td>${element.createdAt}</td>
+                      <td>${element.receive}</td>
+                      <td>${element.subject_name}</td>
+                      <td><button onclick="buttonClick(${element.id})" class="mail_button">상세보기</button></td>
+                  </tr>
+                  <tr>
+                      <td colspan="4"><hr style="width: 100%; height: 0.1px; border: none; background-color: black;"></td>
+                  </tr>`;
+      });
+    });
 }
