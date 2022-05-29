213b1ba05faaaa27fa78f7baa2ebadc5bee82dc3
Crmal <pjm2207@naver.com>
2022-05-29 11:18:39 +0900

    feat: mail 전송 기능 추가

--- static/js/mail.js
+++ static/js/mail.js
@@ -34,33 +34,30 @@ function buttonClick(ID) {
 }
 
 function sendMail() {
+  let email = document
+    .getElementById("receiver")
+    .textContent.replace(/\n/g, "");
+  email = email.replaceAll(" ", "");
   fetch("http://ec2-54-167-22-41.compute-1.amazonaws.com/api/sendMail", {
     method: "POST",
     headers: {
       "X-Access-Token": Cookies.get("X-Access-Token"),
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      professor_email: document.getElementById("sendEmail").value,
-      subject: document.getElementById("sendEmail").value,
-      text: document.getElementById("sendEmail").value,
+      professor_email: email,
+      subject: document.getElementById("title").value,
+      text: document.getElementById("content").value,
       subject_name: "인터넷 프로그래밍",
       receive: "정재희",
     }),
   })
+    .catch((error) => {
+      console.log(error);
+    })
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      var mailbox = document.getElementById("mailbox_list_contents");
-      data.data.mailData.forEach((element) => {
-        mailbox.innerHTML += `<tr align="center">
-                      <td>${element.createdAt}</td>
-                      <td>${element.receive}</td>
-                      <td>${element.subject_name}</td>
-                      <td><button onclick="buttonClick(${element.id})" class="mail_button">상세보기</button></td>
-                  </tr>
-                  <tr>
-                      <td colspan="4"><hr style="width: 100%; height: 0.1px; border: none; background-color: black;"></td>
-                  </tr>`;
-      });
     });
+  window.location.reload();
 }
